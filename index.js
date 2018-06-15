0fe11225dbe615937242bfd1c82e13bf6277caf6
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-15 01:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,9 +45,10 @@ const total = () => {
 const removeFromCart = (name) => {
   for (let i=0; i<cart.length; i++) {
     if (cart[i]['itemName'] === name) {
-      cart.splice(i, 1)
+      var removed = cart.splice(i, 1)
+      return removed
     } else {
-      return "That item is not in your cart."
+      return 'That item is not in your cart.'
     }
   }
 }
