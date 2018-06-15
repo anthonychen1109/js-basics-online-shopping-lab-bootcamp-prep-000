95e3a473b41216fd269464b7309b591410459fb6
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-15 00:21:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
-var cart = [];
+cart = []
 
-function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
-  return cart;
-}
-
-function addToCart(item) {
- // write your code here
- let price = Math.floor(Math.random() * 100) + 1
- let newItem = new Object({ itemName: item, itemPrice: price})
- return `${newItem.itemName} has been added to your cart.`
-}
-
-function viewCart() {
-  // write your code here
-  let list = ''
-  let myCart = getCart()
-  if (myCart.length === 0) {
-    return 'Your shopping cart is empty.'
+const addToCart = (item) => {
+  newItem = {
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 100) + 1
   }
-  else {
-    for (let x=0; x<myCart.length; x++) {
-      list += `you have ${x[itemName]} at ${price}`
+  cart.push(newItem)
+  return `${newItem['itemName']} has been added to your cart.`
+}
+
+const viewCart = () => {
+  list = []
+  counter = 0
+  console.log(cart.length);
+  while(counter < cart.length-1) {
+    list.push(`${cart[counter]['itemName']} at $${cart[counter]['itemPrice']}, `)
+    counter += 1
+  }
+  return `In your cart, you have ${list}and ${cart[cart.length-1]['itemName']} and at $${cart[cart.length-1]['itemPrice']}.`
+}
+
+const total = () => {
+  sum = 0
+  for (let i=0; i<cart.length; i++) {
+    sum += parseInt(cart[i]['itemPrice'])
+  }
+  return sum
+}
+
+const removeFromCart = (name) => {
+  for (let i=0; i<cart.length; i++) {
+    if (cart[i]['itemName'] === name) {
+      cart.splice(i, 1)
+    } else {
+      return 'That item is not in your cart.'
     }
-    return `In your cart, ${list}.`
   }
 }
 
-function total() {
-  // write your code here
-  let myCart = getCart()
-  let total = 0
-  for (let x=0; i<myCart.length; i++) {
-    total += parseFloat(x['price'])
+const placeOrder = (creditCard="") => {
+  if (creditCard.length == "") {
+    return "Sorry, we don't have a credit card on file for you.."
+  } else {
+    my_total = total()
+    cart = []
+    return `Your total cost is $${my_total}, which will be charged to the card ${creditCard}.`
   }
-  return total
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
 }
