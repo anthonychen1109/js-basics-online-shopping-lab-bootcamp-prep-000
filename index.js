964fdb669e5548848fe58b657deb799ba0835afe
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-15 01:44:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,13 +21,15 @@ const addToCart = (item) => {
 const viewCart = () => {
   var list = []
   var counter = 0
-  if (cart.length === 1) {
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  } else if (cart.length === 1) {
     return `In your cart, you have ${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`
   } else {
     while(counter < cart.length-1) {
       list.push(`${cart[counter]['itemName']} at $${cart[counter]['itemPrice']}, `)
       counter += 1
-    }  
+    }
   }
   return `In your cart, you have ${list}and ${cart[cart.length-1]['itemName']} at $${cart[cart.length-1]['itemPrice']}.`
 }
